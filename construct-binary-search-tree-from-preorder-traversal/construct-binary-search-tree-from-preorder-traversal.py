c991492df69154212801da171ea14859db8abe73
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2021-10-14 04:05:03 +0530

    31 ms, (30.20% ). Memory Usage: 13.5 MB ( 75.51%)

--- construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.py
+++ construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.py
@@ -16,16 +16,16 @@ class Solution(object):
         root = TreeNode(preorder[0])
         def helper(preorder,leftmin,rightmax):
             #global cnt
-            if(self.cnt>=len(preorder) or preorder[self.cnt]<leftmin or preorder[self.cnt]>rightmax):
+            if(self.cnt>=len(preorder) or preorder[self.cnt]<leftmin or preorder[self.cnt]>rightmax): #self.cnt>=preorder comes first so that if cnt>len other OR conditions are not checked as they will give error
                 #print("cnt:",self.cnt,preorder[self.cnt])
                 return(None)
             #if(preorder[cnt]>leftmin and preorder[cnt]<rightmax):
             val=preorder[self.cnt]
             #print(val)
-            root=TreeNode(val)
+            root=TreeNode(val) #append value to current root
             self.cnt+=1
-            root.left=helper(preorder,leftmin,val)
-            root.right=helper(preorder,val,rightmax)
+            root.left=helper(preorder,leftmin,val) #BST SO lower bound is carried from prev iteration,upper bound is the current root val 
+            root.right=helper(preorder,val,rightmax) # all values to right are greater than current root val ,with UB starting from infinity
             return(root)
                 
         return(helper(preorder,float("-inf"),float("inf")))
