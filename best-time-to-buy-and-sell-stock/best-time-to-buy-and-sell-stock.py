6e6df86a148db696dfc2113894d7c12d834e538e
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2021-11-10 22:16:55 +0530

    Time: 884 ms (71.64%), Space: 22.5 MB (92.01%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,21 +4,21 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        besttimetobuy=[prices[0]]*len(prices)
-        #besttimetobuy[0]=prices[0]
-        
+        maxval=0
+        diff=0
         for i in range(1,len(prices)):
-            if(prices[i]<besttimetobuy[i-1]):
-                besttimetobuy[i]=prices[i]
+            diff+=prices[i]-prices[i-1]
+            if(diff<0):
+                diff=0
             else:
-                besttimetobuy[i]=besttimetobuy[i-1]
-        #print(besttimetobuy)
-        
-        profit=[0]*len(prices)
-        for i in range(1,len(prices)):
-            if(prices[i]-besttimetobuy[i]>profit[i-1]):
-                profit[i]=prices[i]-besttimetobuy[i]
-            else:
-                profit[i]=profit[i-1]
-        return(profit[len(profit)-1])
+                maxval=max(maxval,diff)
+        return(maxval)
+        # besttimetobuy=prices[0]
+        # profit=0
+        # for i in range(1,len(prices)):
+        #     if(prices[i]<besttimetobuy):
+        #         besttimetobuy=prices[i]
+        #     if(prices[i]-besttimetobuy>profit):
+        #         profit=prices[i]-besttimetobuy
+        # return(profit)
         
