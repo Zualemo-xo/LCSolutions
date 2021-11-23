0954c311762950e3738af2a9d24f3164a73f48d0
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2021-11-24 03:48:10 +0530

    Time: 372 ms (21.93%), Space: 79.1 MB (30.97%) - LeetHub

---
+++ largest-component-size-by-common-factor/largest-component-size-by-common-factor.java
@@ -0,0 +1,60 @@
+class Solution {
+	private class UnionFind {
+		private int[] parent;
+
+		private UnionFind(int n){ //Link every number till max with itself(by assigning it to its index pos)
+			parent = new int[n];
+			for(int i=0;i<n;i++){
+				parent[i] = i;
+			}
+		}
+		private int getAbsoluteParent(int i){
+			if(parent[i]==i){
+				// absolute parent
+				return i;
+			}
+			parent[i]=getAbsoluteParent(parent[i]); //recusre till main parent ele is found
+			return parent[i];
+		}
+
+		private void unify(int i, int j){
+			int absoluteParentI = getAbsoluteParent(i);
+			int absoluteParentJ = getAbsoluteParent(j);
+			if(absoluteParentI!=absoluteParentJ){
+				// 7->2, 21->2 (7,21)
+				parent[absoluteParentJ] = absoluteParentI;
+			}
+		}
+
+	}
+
+	public int largestComponentSize(int[] nums) {
+		int max=0;
+        //Find max
+		for(int el:nums) {
+			max = Math.max(el, max);
+		}
+		UnionFind uf = new UnionFind(max+1); //Assign parent
+
+		for(int i:nums) {
+			for(int j=2;j*j<=i;j++) {
+				if(i%j==0){ // j is a factor of i
+					uf.unify( j,i); // i=21, j=3
+					uf.unify(i/j,i); // (21,3), (21,21/3(7))
+				}
+			}
+		}
+		//<Parent, Freq>
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int maxComponet = 0;
+		for(int el:nums){
+			int parent = uf.getAbsoluteParent(el);
+			map.put(parent, map.getOrDefault(parent, 0) + 1);
+			maxComponet= Math.max(maxComponet, map.get(parent));
+		}
+
+
+
+		return maxComponet;
+	}
+}
