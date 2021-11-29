4621d8924a7e1f1dd2113cd191ab6592e9c7d7a0
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2021-11-30 03:56:56 +0530

    Time: 204 ms (63.78%), Space: 22.3 MB (27.30%) - LeetHub

---
+++ accounts-merge/accounts-merge.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/accounts-merge/discuss/1601960/C%2B%2BPython-Simple-Solution-w-Images-and-Explanation-or-Building-Graph-and-DFS
+class Solution:
+    def accountsMerge(self, A):
+        G, seen, ans = defaultdict(list), set(), []
+
+        for acc in A:
+            for i in range(2,len(acc)):
+                G[acc[i]].append(acc[i-1])
+                G[acc[i-1]].append(acc[i])
+
+        def dfs(email):
+            seen.add(email)
+            emailList = [email]
+            for E in G[email]:
+                if E not in seen:
+                    emailList.extend(dfs(E))
+            return emailList
+        for acc in A:
+            if acc[1] not in seen:
+                ans.append([acc[0]] + sorted(dfs(acc[1])))
+        return ans
+
+        
