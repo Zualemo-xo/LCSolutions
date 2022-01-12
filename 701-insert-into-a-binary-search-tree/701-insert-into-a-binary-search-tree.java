95908185fa8bbd79b4d48e6f28a0c4ae60673b60
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-01-13 04:11:00 +0530

    Time: 0 ms (100.00%), Space: 51.8 MB (7.05%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -13,41 +13,89 @@
  *     }
  * }
  */
+// TC: O(logn) -> O(n)
+// SC: O(logn)
+//Recursively
 class Solution {
-    boolean added=false;
-    public TreeNode helper(TreeNode root,int val)
-    {
-        if(root!=null && added==false)
-        {
-
-        //System.out.println(root.val);
-        if(root.val<val)
-        {
-            if(root.right!=null)
-            helper(root.right,val);
-            
-            else if(root.right==null)
-            {root.right=new TreeNode(val);
-            added=true;}
-        }
-        
-        else if(root.val>val)
-        {
-            if(root.left!=null)
-            helper(root.left,val);
-            
-            else if(root.left==null)
-            {root.left=new TreeNode(val);
-            added=true;}
-        }
-            
-        }
-        return(root);
-    }
-    public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root==null)
-            return(new TreeNode(val));
-        TreeNode ans=helper(root,val);
-        return(ans);
-    }
+	public TreeNode insertIntoBST(TreeNode root, int val) {
+		if(root==null){
+			return new TreeNode(val);
+		} else if(root.val > val){
+			root.left = insertIntoBST(root.left, val);
+		} else{
+			root.right = insertIntoBST(root.right, val);
+		}
+		return root;
+	}
 }
+
+//Iterative
+//  class Solution {
+//      public TreeNode insertIntoBST(TreeNode root, int val) {
+//          if(root == null)
+//              return new TreeNode(val);
+
+//          TreeNode tmp = root;
+//          while(true){
+//              //go to right sub tree
+//              if(tmp.val < val){
+//                  if(tmp.right == null){
+//                      tmp.right = new TreeNode(val);
+//                      break;
+//                  }else{
+//                      tmp = tmp.right;
+//                  }
+//              }
+//              //go to left sub tree
+//              else{
+//                  if(tmp.left == null){
+//                      tmp.left = new TreeNode(val);
+//                      break;
+//                  }else{
+//                      tmp = tmp.left;
+//                  }
+//              }
+//          }
+//          return root;
+//      }
+//  }
+
+// //My method
+// class Solution {
+//     boolean added=false;
+//     public TreeNode helper(TreeNode root,int val)
+//     {
+//         if(root!=null && added==false)
+//         {
+
+//         //System.out.println(root.val);
+//         if(root.val<val)
+//         {
+//             if(root.right!=null)
+//             helper(root.right,val);
+            
+//             else if(root.right==null)
+//             {root.right=new TreeNode(val);
+//             added=true;}
+//         }
+        
+//         else if(root.val>val)
+//         {
+//             if(root.left!=null)
+//             helper(root.left,val);
+            
+//             else if(root.left==null)
+//             {root.left=new TreeNode(val);
+//             added=true;}
+//         }
+            
+//         }
+//         return(root);
+//     }
+//     public TreeNode insertIntoBST(TreeNode root, int val) {
+//         if(root==null)
+//             return(new TreeNode(val));
+//         TreeNode ans=helper(root,val);
+//         return(ans);
+//     }
+// }
