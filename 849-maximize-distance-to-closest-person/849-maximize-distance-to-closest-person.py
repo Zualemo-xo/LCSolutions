3352e47df2e92eb3de2383faf2290587e386afe8
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-01-17 02:56:56 +0530

    Time: 172 ms (35.31%), Space: 15 MB (16.71%) - LeetHub

---
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        dist=-1
+        sit,prev=0,0
+        for i in range(0,len(seats)):
+            if( (i-prev>sit and seats[i]==0 and i+1==len(seats) )or (prev==0 and seats[0]==0 and seats[i]==1)):
+                print("ff",i)
+                sit=i-prev
+                dist=i-prev
+                prev=i
+            elif(i-prev>dist and seats[i]==1 ):
+                #print(prev,i)
+                sit=max(sit,max(1,(i-prev)//2) )
+                dist=i-prev
+                prev=i
+                #print("wtf")
+            if(seats[i]==1):
+                prev=i
+            print("SIT:",sit) 
+        print("SITANS:",sit)
+        return(sit)
+                
+                
