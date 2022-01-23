c4cf6f54838360119a2b65cc39da26def1b26977
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-01-24 03:27:53 +0530

    Time: 27 ms, Memory: 13.4 MB - LeetHub

---
+++ powx-n/powx-n.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        nc=n
+        ans=1
+        if(n<0):
+            n*=-1
+        while(n):
+            
+            if(n%2==0):
+                x=x*x
+                n=n/2
+            else:
+                ans=x*ans
+                n-=1
+        if(nc<0):
+            ans=1/ans
+        return(ans)
+                
+            
+        
