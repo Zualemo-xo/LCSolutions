45ae955f71aeef007748e06090fce7f1a8099856
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-02-08 11:41:16 +0530

    Time: 45 ms (7.31%), Space: 13.5 MB (35.58%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,10 +1,14 @@
 
 class Solution(object):
     def addDigits(self, num):
-        l=list(str(num))
-        while(num>9):
-            num=0
-            for i in range(0,len(l)):
-                num+=int(l[i])
-            l=list(str(num))
-        return(num)
+        return 1 + (num - 1) % 9 if num else 0
+        # Testing pattern between 0 and 100..we observe 0 , then 1-9 repeating
+        # for num in range(0,100):
+        #     l=list(str(num))
+        #     while(num>9):
+        #         num=0
+        #         for i in range(0,len(l)):
+        #             num+=int(l[i])
+        #         l=list(str(num))
+        #     #return(num)
+        #     print(num)
