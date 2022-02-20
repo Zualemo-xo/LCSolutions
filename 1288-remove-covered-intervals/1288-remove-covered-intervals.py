beb2eaff541aa5aa1e1d542a08d054e69680c269
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-02-21 01:37:20 +0530

    Time: 104 ms (58.33%), Space: 14 MB (41.67%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -5,8 +5,8 @@ class Solution(object):
         :rtype: int
         """
         cnt=0
-        for i in range(0,len(intervals)):
-            for j in range(0,len(intervals)):
+        for i in xrange(0,len(intervals)):
+            for j in xrange(0,len(intervals)):
                 #print(intervals[i],intervals[j])
                 if(intervals[j][0]<=intervals[i][0] and intervals[i][1]<=intervals[j][1] and i!=j):
                     cnt+=1
