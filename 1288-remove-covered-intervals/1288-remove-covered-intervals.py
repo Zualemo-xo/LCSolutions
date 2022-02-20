312fc0e4438eaa79fab7dcd60a76ccf2ddf67983
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-02-21 01:37:05 +0530

    Time: 204 ms (8.33%), Space: 13.8 MB (88.33%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -1,11 +1,15 @@
-class Solution:
-    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+class Solution(object):
+    def removeCoveredIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
         cnt=0
         for i in range(0,len(intervals)):
             for j in range(0,len(intervals)):
                 #print(intervals[i],intervals[j])
                 if(intervals[j][0]<=intervals[i][0] and intervals[i][1]<=intervals[j][1] and i!=j):
-                    #print(intervals[i],intervals[j])
                     cnt+=1
                     break
         return(len(intervals)-cnt)
+        
