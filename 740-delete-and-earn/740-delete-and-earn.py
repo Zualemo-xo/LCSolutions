ca74a91edd78f23eb5c8ff125f74b82b9c7e342a
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-03-06 02:16:42 +0530

    Time: 47 ms (78.98%), Space: 14.3 MB (13.24%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def deleteAndEarn(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #bottom up dp
+        dp=[0]*(max(nums)+1)
+        #nums.sort()
+        totnums=defaultdict(int)
+        for i in nums:
+            totnums[i]+=i
+        #base case
+        dp[0]=totnums[0]
+        dp[1]=totnums[1]
+        #i refers to no we gonna select, not index
+        # we iterate from case of selecting number '0' till max number in given array
+        for i in range(2,max(nums)+1):
+            dp[i]=max(dp[i-1],dp[i-2]+totnums[i]) #either select this no and combine with i-2 or select i-1 and not consider current no 'i'.
+        #print(dp)
+        #print(totnums)
+        return(dp[-1])
