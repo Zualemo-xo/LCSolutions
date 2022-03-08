11948bf703bc3034e87b4b6e8e68fd0f957be234
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-03-08 12:54:01 +0530

    Time: 71 ms (54.25%), Space: 17.4 MB (24.18%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -1,21 +1,22 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def detectCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        #print(head)
-        l={}
-        while(head):
-            if(head not in l):
-                l[head]=head
-            #print(head.val)
-            else:
-                return(head)
-            head=head.next
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        sp=fp=newp=head
+        #detect cycle
+        while(True):
+            if(fp==None or fp.next==None):
+                return(None)
+            sp=sp.next
+            fp=fp.next.next
+            if(sp==fp):
+                break
+        #find point by initializing a newpointer frmstart and continuing sp interation
+        while(newp!=sp):
+            newp=newp.next
+            sp=sp.next
+        return(newp)
