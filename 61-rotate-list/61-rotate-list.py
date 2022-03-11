f7cce98a2acdd884e9b2f4d47ab3b0e1960f83fa
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-03-11 12:14:41 +0530

    Time: 70 ms (19.07%), Space: 14.1 MB (26.94%) - LeetHub

--- 61-rotate-list/61-rotate-list.py
+++ 61-rotate-list/61-rotate-list.py
@@ -9,21 +9,26 @@ class Solution:
             return(head)
         cnt=0
         temp=head
+        
         while(temp!=None):
             temp=temp.next
             cnt+=1
         k%=cnt
-        while(k>0):
-            k-=1
-            prev=ListNode(-1)
-            prev.next=head
-            temp=head
-            while(temp!=None):
-                if(temp.next==None): #break last node and make it head
-                    prev.next=None
-                    temp.next=head
-                    head=temp
-                    break
-                temp=temp.next
-                prev=prev.next
+        if(k==0):
+            return(head)
+        #optimizing to O(n) from O(n*k)
+        temp=head
+        c=0
+        while(temp!=None):
+            c+=1
+            print(c)
+            if(c==cnt-k):
+                t=rot=temp.next
+                temp.next=None
+                while(t.next!=None): #break cnt-k-1 node and make it head
+                    t=t.next
+                #at the last node connect to head
+                t.next=head
+                head=rot
+            temp=temp.next
         return(head)
