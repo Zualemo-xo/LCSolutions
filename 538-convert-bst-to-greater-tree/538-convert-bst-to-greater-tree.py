3b5537bfb0dc638c83a18820e01cfc29ae6931e6
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-04-17 00:23:35 +0530

    Time: 65 ms (95.12%), Space: 17.9 MB (8.94%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.py
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.py
@@ -1,43 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-cnt=0
-class Solution:
-    
-    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        #1)obtain inorder list
-        inorder=[]
-        def helper(root):
-            if(root==None):
-                return()
-            helper(root.left)
-            inorder.append(root.val)
-            helper(root.right)
-            
-        helper(root)
-        #print(inorder)
-        #2)Get postfix sum , later assgn to respective values using 'cnt' variable
-        postfixsum=[0]*len(inorder)
-        for i in range(len(inorder)-1,-1,-1):
-            if(i==len(inorder)-1):
-                postfixsum[i]=inorder[i]
-            else:
-                postfixsum[i]=inorder[i]+postfixsum[i+1]
-        print(postfixsum)
-        def assign(root):
-            global cnt
-            if(root==None):
-                return()
-            assign(root.left)
-            #print(root.val,postfixsum[cnt],cnt)
-            root.val=postfixsum[cnt]
-            cnt+=1
-            assign(root.right)
-            
-        assign(root)
-        global cnt
-        cnt=0
-        return(root)
+class Solution(object):
+    def __init__(self):
+        self.total = 0
+
+    def convertBST(self, root):
+        if root is not None:
+            self.convertBST(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.convertBST(root.left)
+        return root
+        
