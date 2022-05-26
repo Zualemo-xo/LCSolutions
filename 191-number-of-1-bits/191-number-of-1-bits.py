7a38d9fc2f51aad1ffc2c574e08c4fdc8ecb9909
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-05-26 13:48:43 +0530

    Time: 57 ms (17.77%), Space: 13.8 MB (49.85%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,3 +1,9 @@
-class Solution(object):
-    def hammingWeight(self, n):
-        return(collections.Counter( str(bin(n))[2:])['1'])
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        ans=0
+        while(n!=0):
+            bit=n & 1 #Find out if the end digit is 1 or 0 using AND
+            if(bit==1):
+                ans+=1
+            n=n>>1 #REMOVE last digit to proceed with next iteration
+        return(ans)
