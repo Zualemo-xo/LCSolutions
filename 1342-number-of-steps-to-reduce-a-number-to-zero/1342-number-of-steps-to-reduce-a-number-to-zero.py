58fe65364a4b36cc16efa2c88837c9274aa19cdb
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-05-27 14:22:15 +0530

    Time: 67 ms (5.24%), Space: 13.8 MB (96.14%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,10 +1,12 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        #Simulate
+        #Bit Manipulation: to remove 1 at the left most--1 turn ,other 1 -- 2 turns, 0 -- 1 turns
         cnt=0
-        print("New")
         while(num!=0):
-            cnt+=1
-            print(bin(num))
-            num=num//2 if num%2==0 else num-1
+            if(num==1): #Exception case
+                cnt+=1
+                break
+            x=num&1
+            cnt=cnt+2 if x==1 else cnt+1
+            num>>=1
         return(cnt)
