413e17567cd68a4b5e9e69890681502d48420183
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-06-01 13:13:11 +0530

    Time: 46 ms (24.11%), Space: 13.8 MB (13.89%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -4,5 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        #not prefixS 1liner
-        return(sum(nums[0:i+1]) for i in range(0,len(nums)) )
+        #prefixS
+        for i in range(1,len(nums)):
+            nums[i]+=nums[i-1]
+        return(nums)
