c0ac92065d914119388462ae6aa825311676a2e3
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-06-03 01:49:57 +0530

    Time: 272 ms (33.14%), Space: 14.2 MB (92.28%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,20 +1,31 @@
-class Solution(object):
-    def isBipartite(self, graph):
-        color = {}
-        def dfs(pos):
-            for i in graph[pos]:
-                if i in color:
-                    if color[i] == color[pos]:
-                        return False
-                else:
-                    color[i] = 1 - color[pos]
-                    if not dfs(i):
-                        return False
-            return True
-        for i in range(len(graph)):
-            if i not in color:
-                color[i] = 0
-                if not dfs(i):
-                    return False
-        return True
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        color=defaultdict(bool)
+        #Start BFS AT A POINT WHICH HAS MOST CONNECTIONS
+        maxl,startpoint=0,0
+        for pt,i in enumerate(graph):
+            if(len(i)>maxl):
+                startpoint=pt
+                maxl=len(i)
+                #print(pt,i)
+        #print(maxl,pt)
+        color[startpoint]=True #key :value here are node:color 1 (True) or 2 (False)
         
+        #BFS
+        queue=deque([startpoint])
+        visited=set()
+        while(len(queue)!=0):
+        #for node in range(0,len(graph)):--iteration of all nodes in order instead of BFS
+            node=queue.popleft()
+            #print(node)
+            for neighbor in graph[node]:
+                if(neighbor not in color):
+                    color[neighbor]=not color[node] #different color to original node
+                    queue.append(neighbor)
+                    
+                elif(color[neighbor]==color[node]): #when neighbor also has same color coordinate, it means graph is !bipartite
+                    return(False)
+                
+        
+        return(True)
+                
