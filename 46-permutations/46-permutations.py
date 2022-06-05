d8545ba3aaebc0f3a65cebfe7dd5b305aeb5bb96
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-06-06 02:17:31 +0530

    Time: 34 ms (56.83%), Space: 13.7 MB (38.85%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        n=len(nums)
+        self.ans=[]
+        # whe check and add different positions into curr
+        def backtrack(curr,pos):
+            #print(curr,pos)
+            #break condition
+            if(len(curr)==n):
+                tans=[]
+                for i in curr:
+                    tans.append(nums[i])
+                #if tans not in self.ans: NEEDED IN LC)47. PERMUTATIONS 2
+                self.ans.append(tans)
+                return
+
+            #while(i%n!=pos%n):
+            #loop from first ele and add if not already a position
+            for i in range(0,n):
+                if(i not in curr):
+                    #print(i%n,pos%n)
+                    curr.append(i)
+                    backtrack(curr,i+1)
+                    curr.pop()
+                    #i+=1
+        
+        backtrack([],0)
+        return(self.ans)    
