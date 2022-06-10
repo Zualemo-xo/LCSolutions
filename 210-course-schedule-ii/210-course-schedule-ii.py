337ffd086fb5d54b3dfb5ac43d838435cba95c66
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-06-10 17:35:21 +0530

    Time: 231 ms (10.03%), Space: 15.6 MB (67.84%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        #TOPO SORT
+        adj_list=[[] for i in range(numCourses)]
+        #outgoing_cnt=defaultdict(int)
+        incoming_cnt=defaultdict(int)
+        #print(adj_list)
+        
+        #Creation of Adj list, count of outgoing nodes
+        for i in prerequisites:
+            adj_list[i[1]].append(i[0])
+            #outgoing_cnt[i[1]]+=1
+            incoming_cnt[i[0]]+=1
+        #print(outgoing_cnt)
+        #print(adj_list)
+        
+        #Store nodes with no outgoing edges into queue
+        queue=deque([])
+        ans=[]
+        for i in range(numCourses):
+            if(i not in incoming_cnt):
+                print(i)
+                queue.append(i)
+        #Main topo sort processing
+        while(len(queue)!=0):
+            print(queue)
+            course=queue.popleft()
+            for dep in adj_list[course]:
+                incoming_cnt[dep]-=1 #we have visited its prerequesite course , hence remove its incoming connection to 'dep'
+                if(incoming_cnt[dep]==0): # we can now take this subject
+                    queue.append(dep)
+            ans.append(course)
+        
+        if(len(ans)!=numCourses):
+            return([])
+        else:
+            return(ans)
+            
+            
+        
+            
+            
