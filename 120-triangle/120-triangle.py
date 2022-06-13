e20f07f5e3caeaa2d10a8a8821f3f8ef0e44d079
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-06-13 16:29:05 +0530

    Time: 93 ms (52.73%), Space: 15.5 MB (17.42%) - LeetHub

---
+++ 120-triangle/120-triangle.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumTotal(self, t: List[List[int]]) -> int:
+        #MEMOIZATION
+        self.dp=[]
+        for i in t:
+            self.dp.append([float("inf")]*len(i))
+        #print(self.dp)
+        
+        def helper(row,pos):
+            if(pos<0 or pos>row):
+                return(float("inf"))
+            if(row==len(t)):
+                return(0)
+            if(self.dp[row][pos]!=float("inf")):
+                return(self.dp[row][pos])
+            
+            else:
+                mina=min(helper(row+1,pos), helper(row+1,pos+1) )
+                self.dp[row][pos]=mina+t[row][pos]
+                return(self.dp[row][pos])
+            
+            
+        return(helper(0,0))
+        #x=helper(0,0,0)
+        #print(self.dp)
+
+            
