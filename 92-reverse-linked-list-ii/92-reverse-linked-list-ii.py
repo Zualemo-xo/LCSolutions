1230e7439c8786823a7fd155554c1678111797ac
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-06-15 19:08:38 +0530

    Time: 45 ms (5.50%), Space: 13.8 MB (32.06%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -1,24 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        # TC O(2N) SC 0(K) K=RIGHT-LEFT
-        stack=deque()
-        temp=head
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        """
+        :type head: ListNode
+        :type left: int
+        :type right: int
+        :rtype: ListNode
+        """
+        #Recursion
+        curr=head
         cnt=0
-        while(temp!=None):
-            cnt+=1
-            if(cnt>=left and cnt<=right):
-                stack.append(temp.val)
-            temp=temp.next
-        temp=head
-        cnt=0
-        while(temp!=None):
-            cnt+=1
-            if(cnt>=left and cnt<=right):
-                temp.val=stack.pop()
-            temp=temp.next
+        def recursion(swapvalue,curr,cnt,right):
+            while(curr!=None):
+                cnt+=1
+                if(cnt==right):
+                    temp=curr.val
+                    curr.val=swapvalue
+                    return(temp)
+                else:
+                    curr=curr.next
+        
+        while(curr!=None):
+                cnt+=1
+                if(cnt<left):
+                    curr=curr.next
+                
+                elif(cnt>=left and cnt<=(left+right)/2): #perform recursive swapping for half of the linked list , the 2nd half will be automatically sorted
+                    curr.val=recursion(curr.val,curr,cnt-1,right+left-cnt)
+                    curr=curr.next
+                else:
+                    curr=curr.next
         return(head)
+        
+
+            
