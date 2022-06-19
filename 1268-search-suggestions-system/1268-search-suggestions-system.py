b758978644b134443e2dffb7587a5a2f42d3a1fd
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-06-20 02:31:52 +0530

    Time: 3145 ms (5.05%), Space: 46.2 MB (5.05%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -3,8 +3,6 @@ class TrieNode:
         self.char=char
         self.children={}
         self.is_end=False
-    
-#FK THIS QSN
         
 class Solution(object):
     def suggestedProducts(self, products, searchWord):
@@ -17,7 +15,6 @@ class Solution(object):
         def insert(word):
             node=self.root
             for letter in word:
-                #print(letter,node.children)
                 if(letter in node.children):
                     node=node.children[letter]
                 else:
@@ -26,12 +23,22 @@ class Solution(object):
                     node=new_node
             node.is_end=True
             
-    
+        def suggestion(node,suggest_word):
+            if(node.is_end==True and self.count<3 and suggest_word not in self.ans):
+                self.ans.append(suggest_word)
+                self.count+=1
+                #return #We shd not RETURN AND CONTINUE ITERATING TO FIND OTHER LRX SMALL WORDS IN ITS CHILDREN
+                
+            for i in range(97,123):
+                if(self.count>=3):
+                    break
+                if(chr(i) in node.children):
+                    #RECURSIVELY search till this lex smallest word ends
+                    suggestion(node.children[chr(i)],suggest_word+chr(i))    
+            return
         
         def search(part_word):
             node=self.root
-            #print(node.children)
-            #print(part_word)
             #traverse till last char in word
             for letter in part_word:
                 #print(letter)
@@ -44,35 +51,6 @@ class Solution(object):
             #see what are the possible traversals after this
             #or letter in node.children:
             #CALL A FN TO SEE WHAT WORDS CAN BE FORMED from current 'node'
-            #print(node.children, part_word)
-            def suggestion(node,suggest_word):
-                #print(suggest_word , node.children)
-                #print("wtf",self.count)
-
-                # elif(node.is_end==True): #we dont need this word so just return
-                #     #print("hi")
-                #     return
-                if(node.is_end==True and self.count<3 and suggest_word not in self.ans):
-                    #print("ANS:",suggest_word)
-                    self.ans.append(suggest_word)
-                    #print(suggest_word)
-                    self.count+=1
-                    #return
-                
-                for i in range(97,123):
-                    #print(chr(i))
-                    if(self.count>=3):
-                        #print("fbvbgbgb")
-                        break
-                    #print(node.children)
-                    if(chr(i) in node.children):
-                        #print(chr(i),suggest_word)
-                        suggestion(node.children[chr(i)],suggest_word+chr(i))             
-                    #print(self.ans)
-
-                
-                return
-        
             suggestion(node,part_word)
             return
                         
@@ -87,15 +65,11 @@ class Solution(object):
         self.ans=[]
         self.count=0
         for i in range(0,len(searchWord)):
-            #print(searchWord[0:i+1],len(searchWord))
             search(searchWord[0:i+1])
-            print("ANSiteration",i,self.ans)
-            #self.ans.sort()
             final_ans.append(self.ans)
             self.ans=[]
-            self.count=0 #OPEN EYES AND USE EITHER CNT OR COUNT AS VAR NAME DAMNIT
-            
-        #print(final_ans)
+            self.count=0 #OPEN EYES AND USE EITHER CNT OR COUNT AS VAR NAME 
+
         return(final_ans)
 
             
