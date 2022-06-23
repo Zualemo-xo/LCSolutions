e133e0b29cefe4872f035331999cfafd0a06792e
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-06-24 03:27:17 +0530

    Time: 1310 ms (20.66%), Space: 20.1 MB (51.17%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -0,0 +1,31 @@
+import heapq
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        courses.sort(key=lambda x:x[1]) #Sort by deadline
+        selected=[]
+        heapq.heapify(selected)
+        print(courses)
+        duration=0
+        ans=0
+        
+        for coursetime,deadline in courses:
+            #print(selected)
+            if(duration+coursetime<=deadline):
+                ans+=1
+                duration+=coursetime
+                heapq.heappush(selected,-coursetime) #- to implement maxheap
+                continue
+                #print(duration)
+            #If current courseduration is less than max courseduration encountered till now,
+            # pop out that maxCD and push current courseduration instead, ENSURES WE GET MAX no of courses
+            #TC : [[10,12],[6,15],[1,12],[3,20],[10,19]]
+            elif(len(selected)==0):
+                continue
+            largest=heapq.heappop(selected)
+            if(coursetime<=-largest and duration+coursetime-(-largest)<=deadline):
+                duration=duration-(-largest)+coursetime
+                heapq.heappush(selected,-coursetime)
+            else:
+                heapq.heappush(selected,largest) 
+        return(ans)
+            
