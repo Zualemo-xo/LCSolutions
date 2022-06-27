49360c831316ad4da832b1f2015ff68938e0380d
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-06-28 04:32:51 +0530

    Time: 387 ms (73.81%), Space: 23.8 MB (14.72%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,19 +1,38 @@
-class Solution {
-    public int maxScore(int[] cardPoints, int k) {
-        int[] dp = new int[k + 1];
-        //   this dp array stores the total points when taking i cards from left.
-        //   since we could take 0 - k cards from left ,the length of the dp array would be k+1.
+class Solution(object):
+    def maxScore(self, points, k):
+        """
+        :type cardPoints: List[int]
+        :type k: int
+        :rtype: int
+        """
 
-        for (int i = cardPoints.length - 1; i >= cardPoints.length - k; i--) {
-            dp[0] += cardPoints[i];
-        }
-        int max_points = dp[0];
-
-        for (int i = 1; i < k + 1; i++) {
-            dp[i] = dp[i - 1] + cardPoints[i - 1] - cardPoints[cardPoints.length - k + i - 1];
-            max_points = Math.max(max_points, dp[i]);
-        }
-
-        return max_points;
-    }
-}
+        #sliding window of size n-k to see what elements to not consider
+        
+        sw=deque([])
+        maxl=len(points)-k
+        if(maxl==0): #EDGE C
+            return(sum(points))
+        
+        cursum=0
+        minsum=float("inf")
+        for i in range(0,len(points)):
+            if(len(sw)<maxl):
+                sw.append(points[i])
+                cursum+=points[i]
+                
+            elif(len(sw)==maxl):
+                minsum=min(cursum,minsum)
+                ele=sw.popleft()
+                cursum-=ele
+                sw.append(points[i])
+                cursum+=points[i]
+                
+        minsum=min(cursum,minsum)
+        return(sum(points)-minsum)
+                
+                
+            
+        
+        
+        
+        
