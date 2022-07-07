1cbad13a100c1b318afa1afa4b50760f074441d9
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-07-07 16:08:07 +0530

    Time: 79 ms (30.59%), Space: 14.6 MB (49.44%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,11 +1,14 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        #0-1 KNAPSACK TC: O(N.M) SC:O(N.M)
-        @cache
+        #0-1 KNAPSACK MEMOIZATION TC: O(N.M) SC:O(N.M)
+        #memo[posS1][PosS2]
+        self.memo=[[-1 for i in range(len(s2)+1)] for j in range(len(s1)+1)]
         def dp(posS1,posS2):
             posS3=posS1+posS2
-            #print(posS1,posS2,posS3)
-            #print(posS1,posS2,posS3,s1[posS1],s2[posS2], s3[posS3])
+            #Memoization
+            if(self.memo[posS1][posS2]!=-1):
+                return(self.memo[posS1][posS2])
+                
             if( posS3==len(s3) ):
                 if( posS2==len(s2) and posS1==len(s1) ):
                     #print("rgb")
@@ -18,17 +21,17 @@ class Solution:
                         x=  dp(posS1+1,posS2)
                     else:
                         x=False
-                    return(x)
+                    
                 
                 elif( posS1>=len(s1) ):
                     if(s2[posS2]==s3[posS3]):
                         x=  dp(posS1,posS2+1)
                     else:
                         x= False
-                    return(x)
+                    
                 
                 elif(s2[posS2] != s3[posS3] and s1[posS1] != s3[posS3]):
-                    return(False)
+                    x=False
                 else:
                     if(s1[posS1]==s2[posS2] and s2[posS2]==s3[posS3]): #Both strings could take this string
                         x=  dp(posS1,posS2+1) | dp(posS1+1,posS2)
@@ -37,7 +40,8 @@ class Solution:
                     elif(s2[posS2]==s3[posS3]):
                         x=  dp(posS1,posS2+1)
                     #print(posS1,posS2,posS3,s1[posS1],s2[posS2], s3[posS3])    
-                    return(x)
+                self.memo[posS1][posS2]=x
+                return(x)
         
         if(len(s1)+len(s2)!=len(s3)): # Edge
             return(False)
