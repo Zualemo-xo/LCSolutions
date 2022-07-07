4a09dc9cef2c09cd50d71bcd4a632ddd218e60a7
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-07-07 16:04:07 +0530

    Time: 72 ms (40.18%), Space: 15.6 MB (13.40%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,9 +1,9 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        #TRY 0-1 KNAPSACK
+        #0-1 KNAPSACK TC: O(N.M) SC:O(N.M)
         @cache
-        def dp(posS1,posS2,posS3):
-            #d
+        def dp(posS1,posS2):
+            posS3=posS1+posS2
             #print(posS1,posS2,posS3)
             #print(posS1,posS2,posS3,s1[posS1],s2[posS2], s3[posS3])
             if( posS3==len(s3) ):
@@ -15,14 +15,14 @@ class Solution:
             else:
                 if(posS2>=len(s2)): #    Lerngth idn exceeded
                     if(s1[posS1]==s3[posS3]):
-                        x=  dp(posS1+1,posS2,posS3+1)
+                        x=  dp(posS1+1,posS2)
                     else:
                         x=False
                     return(x)
                 
                 elif( posS1>=len(s1) ):
                     if(s2[posS2]==s3[posS3]):
-                        x=  dp(posS1,posS2+1,posS3+1)
+                        x=  dp(posS1,posS2+1)
                     else:
                         x= False
                     return(x)
@@ -31,17 +31,17 @@ class Solution:
                     return(False)
                 else:
                     if(s1[posS1]==s2[posS2] and s2[posS2]==s3[posS3]): #Both strings could take this string
-                        x=  dp(posS1,posS2+1,posS3+1) | dp(posS1+1,posS2,posS3+1)
+                        x=  dp(posS1,posS2+1) | dp(posS1+1,posS2)
                     elif(s1[posS1]==s3[posS3]):
-                        x=  dp(posS1+1,posS2,posS3+1)
+                        x=  dp(posS1+1,posS2)
                     elif(s2[posS2]==s3[posS3]):
-                        x=  dp(posS1,posS2+1,posS3+1)
+                        x=  dp(posS1,posS2+1)
                     #print(posS1,posS2,posS3,s1[posS1],s2[posS2], s3[posS3])    
                     return(x)
         
         if(len(s1)+len(s2)!=len(s3)): # Edge
             return(False)
-        return(dp(0,0,0)) #start selecting with S1
+        return(dp(0,0)) #start selecting with S1
         #dp(False,0,0,0)
                     
                 
