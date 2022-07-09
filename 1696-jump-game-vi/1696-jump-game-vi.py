25b8280695e01f02439b6b976a09ed3e020c522b
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-07-09 17:22:39 +0530

    Time: 3619 ms (5.04%), Space: 35.3 MB (20.60%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -0,0 +1,24 @@
+import heapq
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        # TC: O(N.log(K)) SC: O(K)
+        l=[ [-nums[0],0] ] #Maxheap
+        #      value,index
+        heapq.heapify(l)
+        
+        for i in range(1,len(nums)):
+            while(True):
+                ele=heapq.heappop(l)
+                if(ele[1]<i-k): #This ele is out of range hence its val cannot be considered
+                    continue
+                heapq.heappush( l,[ele[0]-nums[i],i] )
+                heapq.heappush( l,ele )
+                break
+        
+        #print(l)
+        while(True):
+            ele=heapq.heappop(l)
+            if(ele[1]!=len(nums)-1): #This ele is not last ele
+                    continue
+            else:
+                return(-ele[0])
