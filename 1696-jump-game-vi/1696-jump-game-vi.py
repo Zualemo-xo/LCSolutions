78a00377c41173b0b6536a22511303c8ce266fda
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-07-09 18:11:01 +0530

    Time: 841 ms (100.00%), Space: 25.1 MB (41.94%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,21 +1,20 @@
-
 class Solution:
-    def maxResult(self, nums: List[int], k: int) -> int:
-        # Using Monotonic Queue TC: O(N) SC: O(N)
-        q=deque( [0] ) #Monotonic queue stored in lowest to highest index
-        dp=[nums[0]] #Storing final answer
+    def maxResult(self, nums, k):
+        #Optimal code ,similar as minw
+        n = len(nums)
+        score = [0] * n
+        score[0] = nums[0]
+        queue = collections.deque()
+        queue.append(0)
         
-        for i in range(1,len(nums)):
-            #print(q,dp)
-            cnt=0
-            while(q[cnt]<i-k): #pop all elements not falling within the range
-                ele=q.popleft()
-                
-            dp.append( dp[q[0]]+nums[i] )
-            #pop indices which won't be ever chosen in the future, as our new dp is greater than those old dp elements
-            while(len(q)!=0 and dp[q[-1]] <= dp[-1]):
-                q.pop()
-            q.append( i )
-            
-        #print(q,dp)
-        return(dp[-1])
+        for i in range(1, n):
+            # pop the old index
+            while queue and queue[0] < i-k:
+                queue.popleft()
+            score[i] = score[queue[0]] + nums[i]
+            # pop the smaller value
+            while queue and score[i] >= score[queue[-1]]:
+                queue.pop()
+            queue.append(i)
+        return score[-1]
+        
