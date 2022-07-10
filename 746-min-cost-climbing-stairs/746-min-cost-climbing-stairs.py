4326edd12ed49a7153ba9ed1b53e7164e83ea7d4
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-07-10 10:52:43 +0530

    Time: 77 ms (26.22%), Space: 15 MB (10.95%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,13 +1,41 @@
-class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        #bott0m up memory optimized
-        if(len(cost)==2):
-            return(min(cost))
-        x1=cost[0]
-        x2=cost[1]
-        for i in range(2,len(cost)):
-            temp=x2
-            x2=cost[i]+min(x1,x2)
-            x1=temp
-        return(min(x1,x2))
-            
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        # Memization TC: O(N) SC:O(N)
+        n=len(cost)
+        self.memo=[-1 for i in range(n+1)]
+        def dp(pos):
+            if(pos>=n):
+                return(0)
+            if(self.memo[pos]!=-1):
+                return(self.memo[pos])
+            x=cost[pos]+min(dp(pos+1),dp(pos+2))
+            self.memo[pos]=x
+            return(x)
+        
+        ans=min(dp(0),dp(1))
+        return(ans)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
