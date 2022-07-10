3b594aac384e8246d5c9e6c000079fb605456bc0
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-07-10 11:02:01 +0530

    Time: 89 ms (12.81%), Space: 13.6 MB (34.15%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -4,16 +4,16 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        # Botom Up TC: O(N) SC:O(N)
+        # Botom Up Soace Optimized TC: O(N) SC:O(1)
         n=len(cost)
-        dp=[0 for i in range(n)]
+        dp=[0 for i in range(2)]
         dp[0]=cost[0]
         dp[1]=cost[1]
         for i in range(2,n):
-            dp[i]=cost[i]+min(dp[i-1],dp[i-2])
+            dp[i%2]=cost[i]+min(dp[(i-1)%2],dp[(i-2)%2])
             
 
-        ans=min(dp[i],dp[i-1])
+        ans=min(dp[i%2],dp[(i-1)%2])
         return(ans)
     
     
