7c07360899b7c6e1f33d7c1dbc585bb6f131d0f8
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-07-25 02:35:31 +0530

    Time: 137 ms (11.84%), Space: 15.4 MB (97.09%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+#class Solution:
+'''ARCHIT'S SOLN: Time Complexity : O(N), we only traverse each node once, basically doing a standard BFS.
+Space Complexity : O(1), only constant extra space is being used'''
+    
+class Solution(object):
+    def connect(self, root):
+        head = root
+        while root:
+            cur, root = root, root.left
+            while cur:
+                if cur.left:
+                    cur.left.next = cur.right
+                    if cur.next: cur.right.next = cur.next.left
+                else: break
+                cur = cur.next
+                
+        return head
