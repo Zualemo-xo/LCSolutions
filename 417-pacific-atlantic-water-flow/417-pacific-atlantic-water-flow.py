3b1c87c131b00e600cbc53ede05e34fae02a9877
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-09-02 03:42:36 +0530

    Time: 2077 ms (8.16%), Space: 15.4 MB (94.91%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,42 +1,74 @@
-class Solution(object):
-    def pacificAtlantic(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        if not matrix: return []
-        self.directions = [(1,0),(-1,0),(0,1),(0,-1)]
-        m = len(matrix)
-        n = len(matrix[0])
-        p_visited = [[False for _ in range(n)] for _ in range(m)]
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         
-        a_visited = [[False for _ in range(n)] for _ in range(m)]
-        result = []
+        self.ans=[]
+        #memo heights heights[i]
+        self.memo=[[float("inf") for i in range(0,len(heights[0]))] for j in range(0,len(heights))]
+        def isvalid(r,c,x,y):
+            #print(r,c,x,y)
+            if(x<0 or y<0):
+                return(-1) #pacific
+            elif(x>=len(heights) or y>=len(heights[0])):
+                return(-2) #atlantic
+            elif(heights[r][c]<heights[x][y]):
+                return(0) # new point has higher height
+            else:
+                return(1)
         
-        for i in range(m):
-            # p_visited[i][0] = True
-            # a_visited[i][n-1] = True
-            self.dfs(matrix, i, 0, p_visited, m, n)
-            self.dfs(matrix, i, n-1, a_visited, m, n)
-        for j in range(n):
-            # p_visited[0][j] = True
-            # a_visited[m-1][j] = True
-            self.dfs(matrix, 0, j, p_visited, m, n)
-            self.dfs(matrix, m-1, j, a_visited, m, n)
+        def bfs(r,c):
+            queue=deque([[r,c]])
+            x=[-1,1,0,0]
+            y=[0,0,-1,1]
+            pacific=False
+            atlantic=False
+            visited=set((r,c))
+            while(len(queue)!=0):
+                #print(queue)
+                #mEOMIZATION
+                ele=queue.popleft()
+                #visited.add((ele[0],ele[1]))
+                val=self.memo[ele[0]][ele[1]]
+                if(val!=float("inf")):
+                    if(val==0): # Not possible to reach pacific, atlantic
+                        val=0
+                        #continue
+                    elif(val==1):
+                        pacific=True
+                    elif(val==2):
+                        atlantic=True
+                    elif(val==3):
+                        pacific=True
+                        atlantic=True
+                        break
+                        
+                    continue
+                    
+                for i in range(0,4):
+                    tans=isvalid(ele[0],ele[1],ele[0]+x[i],ele[1]+y[i])
+                    if(tans==-2):
+                        atlantic=True
+                    elif(tans==-1):
+                        pacific=True
+                    elif(tans==1):
+                        if( (ele[0]+x[i],ele[1]+y[i]) not in visited):
+                            queue.append([ele[0]+x[i],ele[1]+y[i]])
+                            visited.add((ele[0]+x[i],ele[1]+y[i]))
             
-        for i in range(m):
-            for j in range(n):
-                if p_visited[i][j] and a_visited[i][j]:
-                    result.append([i,j])
-        return result
-                
-                
-    def dfs(self, matrix, i, j, visited, m, n):
-        # when dfs called, meaning its caller already verified this point 
-        visited[i][j] = True
-        for dir in self.directions:
-            x, y = i + dir[0], j + dir[1]
-            if x < 0 or x >= m or y < 0 or y >= n or visited[x][y] or matrix[x][y] < matrix[i][j]:
-                continue
-            self.dfs(matrix, x, y, visited, m, n)
+            if(pacific and atlantic):
+                self.ans.append([r,c])
+                self.memo[r][c]=3
+            elif(pacific):
+                self.memo[r][c]=1
+            elif(atlantic):
+                self.memo[r][c]=2
+            else:
+                self.memo[r][c]=0
+            
+            
+            
         
+        for i in range(0,len(heights)):
+            for j in range(0,len(heights[i])):
+                #print("BFS PTS:",i,j)
+                bfs(i,j)
+        return(self.ans)
