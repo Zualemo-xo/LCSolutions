ddea5cd12e7e29614efbf96a97acb8d636c469b6
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2023-10-13 02:21:21 +0530

    Time: 984 ms (57.81%), Space: 36.3 MB (57.81%) - LeetHub

--- 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.py
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.py
@@ -1,29 +1,29 @@
 class Solution(object):
+    maxCycle = -1
+    def dfs(self, i, Edge, visited, maxCycle ):
+        dist = defaultdict(int)
+        if(i not in visited and Edge[i] !=-1):
+            dist[i] = 1
+            curNode = i
+            #print(curNode)
+            while(Edge[curNode]!=-1 and Edge[curNode] not in visited):
+                visited.add(curNode)
+                dist[Edge[curNode]] = dist[curNode] + 1 
+                curNode = Edge[curNode]
+                
+            if(Edge[curNode] != -1 and Edge[curNode] in dist): # meaning a visited node is encountere ALA cycle found.
+                if(dist[curNode] - dist[Edge[curNode]] + 1  > self.maxCycle): # subtract current node distance with repeating node's dist
+                    self.maxCycle = dist[curNode] - dist[Edge[curNode]] + 1 
     def longestCycle(self, Edge):
         """
         :type edges: List[int]
         :rtype: int
         """
-        maxCycle = -1
-        
         visited = set()
+
         for i in range(0,len(Edge)):
             #dfs
-            dist = defaultdict(int)
-            if(i not in visited and Edge[i] !=-1):
-                dist[i] = 1
-                curNode = i
-                #print(curNode)
-                while(Edge[curNode]!=-1 and Edge[curNode] not in visited):
-                    visited.add(curNode)
-                    dist[Edge[curNode]] = dist[curNode] + 1 
-                    curNode = Edge[curNode]
-                    
-                #print(i,cycleSum,Edge[curNode])    
-                if(Edge[curNode] != -1 and Edge[curNode] in dist): # meaning a visited node is encountere ALA cycle found.
-                    if(dist[curNode] - dist[Edge[curNode]] + 1  > maxCycle): # subtract current node distance with repeating node's dist
-                        #print(visited,curNode,Edge[curNode],dist)
-                        maxCycle = dist[curNode] - dist[Edge[curNode]] + 1 
+            self.dfs(i, Edge, visited, self.maxCycle)
             
-        return(maxCycle)
+        return(self.maxCycle)
         
